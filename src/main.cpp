b8bfec26369ab2f0ce75ed1dbb91ea691f515337
Lenka Mudrova <lxm210@cs.bham.ac.uk>
2014-06-19 17:45:05 +0100

    main method

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 #include <string>
 #include <vector>
-
+#include <math.h>
 #include "task.h"
 #include "scheduler.h"
 
@@ -21,37 +21,93 @@ int main (int argc, char** argv)
     task_count = 10;
   }
 
-  double one_hour = 60 * 6;
-  double task_duration = one_hour / 2.0;
+  int num_prob1 =0;
+  int num_prob2 =0;
 
-  double window_start = 0;
-  double window_end = window_start + (task_duration * task_count * task_count);
-
+  //for(int x=0; x<100; x++) 
+  //{
+  int mind = 2; //minimal duration of tasks
+  int maxd = 30;  //maximal duration of tasks
+  int maxw = 10; //maximum waiting time between two tasks
+  int maxs = 240; //maximum window size
+  int taken=0;
+  int d=0; //duration of task
+  int w=0; //random waiting time between tasks
+  int s; //starting time of task
+  int e; //ending time of task
+  int pw=0;//previous w
+		
+  int sum=0;
+  
   vector<Task*> tasks;
 
-  for (int i = 0; i < task_count; ++i)
-  {
-    
-    tasks.push_back(new Task(i, window_start, window_end, task_duration, s1, s2));
-    //window_start = window_end + 10;
-    //window_end = window_start + (task_duration * 2);
+  srand(time(NULL));
+  for(int i=0;i<task_count;i++)
+  {			
+    w =w+d+(int) round(rand() % maxw) +1;
+    s = w-(int) round(rand() % (maxw*5)); //set random start of task
+    if(s<0)
+      s=0;
+    d = (int) round(rand() % (maxd-mind)+mind);
+    taken=w-s + d;
+    e=(int) round(rand() % (maxs))+taken+s+1;
+    sum=sum+w;
+    cout << s << ";" << e << ";" << d << ";" << w << "\n";
+    tasks.push_back(new Task(i, s, e, d, s1, s2));
   }
-
-
+		
   Scheduler scheduler(&tasks);
-  bool worked = scheduler.solve();
+  bool worked = scheduler.solve(1);
+
+  bool sorted = false;
+  Task * pom = (Task*)NULL; 
+ 
 
   if(worked)  {
     cout<< "Schedule found" << worked << "\n";
     for(auto & tp : tasks) {
-      cout<< "Is start time "<<tp->getExecTime() << " after start? "
+      cout<< "Is start time "<<tp->getExecTime() << " after start? + dur:" << tp->getDuration() << ":" 
+       << (tp->getExecTime() >= tp->getStart()) << "\n";
+    } 
+   /* while(!sorted)
+    {
+      sorted = true;
+      for(int i=0;i<task_count-1;i++)
+      {
+        if(tasks.at(i)->getExecTime() > tasks.at(i+1)->getExecTime())
+        {
+          pom = tasks.at(i);
+          tasks.at(i) = tasks.at(i+1);
+          tasks.at(i+1) = pom;
+          sorted = false;
+        }
+      }
+    }
+    for(int i=0;i<task_count;i++)
+    {  
+       Task * tp = tasks.at(i);
+       for(int j=i+1;j<task_count;j++)
+       {
+         Task * tp2 = tasks.at(j);
+         if(tp->getExecTime()+tp->getDuration() > tp2->getExecTime())
+         {
+           cout << "problem\n";
+           num_prob1++;
+         }
+       }
+    }*/
+  }
+//}
+
+
+  worked = scheduler.solve(2);
+   if(worked)  {
+    cout<< "Schedule found" << worked << "\n";
+    for(auto & tp : tasks) {
+      cout<< "Is start time "<<tp->getExecTime() << " after start? + dur:" << tp->getDuration() << ":" 
        << (tp->getExecTime() >= tp->getStart()) << "\n";
     } 
   }
-  else {
-    cout<< "No schedule found"<< endl;
-  }
-
   return 0;
 
 }
